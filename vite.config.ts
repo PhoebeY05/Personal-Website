731cba2456490c584628f9eb6707c9b4ce6a9782
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2025-11-20 18:56:40 +0800

    fix: Fixes prettier issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // base must be the repo name for GitHub project pages
-  base: '/Personal-Website/',
-  plugins: [react(), tailwindcss()],
+	// base must be the repo name for GitHub project pages
+	base: '/Personal-Website/',
+	plugins: [react(), tailwindcss()],
 });
